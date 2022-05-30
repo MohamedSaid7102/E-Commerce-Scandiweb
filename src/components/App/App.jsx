840e36279420ba0e35d21d866d65c86b9d0e0d1b
Shlef <mohamedsaidwork@gmail.com>
2022-05-30 06:06:47 +0200

    updated routes, cleaned work dir

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,25 +5,43 @@ import PLP from 'pages/PLP';
 import { NavBar } from '../NavBar/NavBar';
 
 import 'assets/style/app.css';
-import PageNotFound from 'pages/PageNotFound';
+import PageNotFound from 'pages/NotFound';
+import request from 'graphql-request';
+import { GET_ALL_PRODUCTS } from 'GraphQL/Queries';
 
 class App extends Component {
   state = {
     selectedCategory: {},
-    selectedCurrency: {
-      label: 'USD',
-      symbol: '$',
-    },
+    selectedCurrency: {},
     cartItems: [],
     cartItemsCount: 2,
+    allProducts: [],
+    techProducts: [],
+    clothesProducts: [],
   };
 
+  componentDidMount() {
+    request('http://localhost:4000', GET_ALL_PRODUCTS).then((data) => {
+      data.categories.forEach((category) => {
+        const categoryName = category.name.toLowerCase() + 'Products';
+        this.setState({ [categoryName]: category.products });
+      });
+    });
+  }
+
+  // Update App state on (category || currency) change in NavBar.
   getSelectedCategoryAndCurrency = (selectedCategory, selectedCurrency) => {
     this.setState({ selectedCategory, selectedCurrency });
   };
 
   render() {
-    const { cartItemsCount, selectedCurrency } = this.state;
+    const {
+      cartItemsCount,
+      selectedCurrency,
+      allProducts,
+      clothesProducts,
+      techProducts,
+    } = this.state;
 
     return (
       <div className="app">
@@ -31,12 +49,43 @@ class App extends Component {
           cartItemsCount={cartItemsCount}
           updateMainStateWithSelection={this.getSelectedCategoryAndCurrency}
         />
-        {/* <PLP selectedCurrency={selectedCurrency} /> */}
         <Routes>
-          {/* <Route
+          <Route
             path="/"
-            element={}
-          ></Route> */}
+            element={
+              <PLP selectedCurrency={selectedCurrency} products={allProducts} />
+            }
+          />
+          <Route
+            path="/all-products"
+            element={
+              <PLP
+                selectedCurrency={selectedCurrency}
+                prodcutsCategory="All"
+                products={allProducts}
+              />
+            }
+          />
+          <Route
+            path="/clothes-products"
+            element={
+              <PLP
+                selectedCurrency={selectedCurrency}
+                prodcutsCategory="Clothes"
+                products={clothesProducts}
+              />
+            }
+          />
+          <Route
+            path="/tech-products"
+            element={
+              <PLP
+                selectedCurrency={selectedCurrency}
+                prodcutsCategory="Tech"
+                products={techProducts}
+              />
+            }
+          />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
