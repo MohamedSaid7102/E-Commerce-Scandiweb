b2f96244f596f1b3b23473339352638accd6afa3
Shlef <mohamedsaidwork@gmail.com>
2022-06-09 18:02:21 +0200

    Redux: setSelectedCurrency action

--- src/Redux/ducks/currencies.js
+++ src/Redux/ducks/currencies.js
@@ -4,6 +4,7 @@ import { GET_CURRENCIES as GET_CURRENCIES_QUERY } from 'GraphQL/Queries';
 
 // Action Types
 const GET_CURRENCIES = 'GET_CURRENCIES';
+const SET_SELECTED_CURRENCY = 'SET_SELECTED_CURRENCY';
 
 export function getCurrencies() {
   return (dispatch) => {
@@ -19,6 +20,23 @@ export function getCurrencies() {
       });
   };
 }
+
+// set current category
+export function setSelectedCurrency(selectedCurrency) {
+  if (typeof selectedCurrency !== 'object')
+    throw new Error(
+      'passed selectedCurrency must be an object!',
+      selectedCurrency
+    );
+
+  return (dispatch) => {
+    dispatch({
+      type: SET_SELECTED_CURRENCY,
+      payload: selectedCurrency,
+    });
+  };
+}
+
 // Reducer
 const initialState = {};
 
@@ -28,6 +46,12 @@ export default (state = initialState, action) => {
       ...state,
       list: action.payload.currencies,
       selectedCurrency: action.payload.selectedCurrency,
+    };
+  }
+  if (action.type === SET_SELECTED_CURRENCY) {
+    return {
+      ...state,
+      selectedCurrency: action.payload,
     };
   }
   return { ...state };
