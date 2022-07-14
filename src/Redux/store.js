09f278d3037d86b1b55874a95fc35026d5231c1d
Shlef <mohamedsaidwork@gmail.com>
2022-07-14 14:01:59 +0200

    Fixed production bugg

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,18 +1,27 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'Redux/ducks';
+import { DEV_MODE } from './config';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = DEV_MODE
+  ? createStore(
+      rootReducer,
+      initialState,
+      compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    )
+  : createStore(
+      rootReducer,
+      initialState,
+      compose(applyMiddleware(...middleware))
+    );
 
 export default store;
