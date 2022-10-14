5f5a95d20934a0d943f28bda61201afd99551279
Shlef <mohamedsaidwork@gmail.com>
2022-10-14 08:15:38 +0200

    Updated .gitignore & config.js

--- src/Redux/config.js
+++ src/Redux/config.js
@@ -1,7 +1,9 @@
-// Just change this
-export const DEV_MODE = false;
-export const PROD_MODE = !DEV_MODE;
+// Just change this, and incase you are in dev mode run npm run start on server, it will run on port 4000, you can find graphQL code in my repo:
 
-export const PORT = DEV_MODE
+// https://github.com/MohamedSaid7102/reactjs---e-commerce-scandiweb.inc
+
+export const IS_DEV_MODE = false;
+
+export const PORT = IS_DEV_MODE
   ? 'http://localhost:4000/'
   : 'https://scandiweb-server-shelf.herokuapp.com/';
